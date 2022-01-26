beb8ee724ab7035fbcd81a217343a8b73af56081
Pramod <pnbhakta@iu.edu>
2022-01-25 23:36:52 -0500

    remapped response senders to include logging

--- epsilon-api-gtw/index.js
+++ epsilon-api-gtw/index.js
@@ -22,6 +22,86 @@ const port = process.env.PORT || 3003;
 
 app.use("*", log.logRequest);
 
+app.use(function (req, res, next) {
+  res.bhejdo = function (status, body = {}, acfBhejnaHai) {
+    log.logResponse(req, res, body, status);
+    if (status == HttpStatus.INTERNAL_SERVER_ERROR) {
+      const msg = "<p>Oops! Something went wrong 😞</p>";
+      if (body && body.stack) {
+        return res.status(status).send({
+          success: false,
+          msg: msg,
+          err: body.stack,
+        });
+      } else if (body) {
+        return res.status(status).send(body);
+      } else {
+        return res.status(status).send({
+          success: false,
+          msg: msg,
+        });
+      }
+    } else if (status == HttpStatus.REQUEST_TOO_LONG) {
+      return res.status(status).send({
+        success: false,
+      });
+    } else {
+      //body.acf = res.acf;
+      if (body && body.msg && req.method == "DELETE") {
+        //body.msg += '';
+      }
+      if (acfBhejnaHai === true) {
+        body.acf = res.acf;
+      }
+      return res.status(status).send(body);
+    }
+  };
+
+  res.errorBhejdo = function (status, body = {}, acfBhejnaHai, custom_error) {
+    log.warn(status + " Error received by errorBhejdo!");
+    if (!custom_error) {
+      if (body && body.stack) {
+        log.logError(body.stack);
+      } else if (body && body.msg) {
+        log.logError(body.msg);
+      } else {
+        log.logError(
+          "Error toh aaya hai, par jabh tak bhejoge nai toh dikhau kaise? Please use the last parameter(Error Object) to send an error object or atleast include error in body.msg(string) or body.stack(string)."
+        );
+      }
+    } else {
+      if (custom_error.message && custom_error.stack)
+        log.logError(custom_error);
+      else if (typeof custom_error == "string")
+        log.logCustomError(custom_error);
+      else if (typeof custom_error == "object")
+        log.logCustomError(JSON.stringify(custom_error));
+      else
+        log.logError(
+          "Custom error toh daal diye ho par atleast Error Object bana lo ya toh string ya toh Object, yeh kya bavaseer bhej diye ho"
+        );
+    }
+
+    return res.bhejdo(status, body, acfBhejnaHai);
+  };
+
+  res.bufferBhejdo = function (buffer, fileName) {
+    res.setHeader("Content-Disposition", contentDisposition(fileName));
+    const status = HttpStatus.OK;
+    log.logResponse(
+      req,
+      res,
+      {
+        bufferSize: buffer.length,
+        fileName: fileName,
+      },
+      status
+    );
+    return res.status(status).send(buffer);
+  };
+  next();
+});
+
 let corsOptionsDelegate = function (req, callback) {
   let corsOptions;
   if (process.env.WHITELIST.indexOf(req.header("Origin")) !== -1) {
