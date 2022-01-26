9c276bee5baa1043ac3c4e083cee90f667b0ee4b
Pramod <pnbhakta@iu.edu>
2022-01-25 23:27:38 -0500

    more router functions

--- epsilon-api-gtw/index.js
+++ epsilon-api-gtw/index.js
@@ -20,9 +20,24 @@ global.app_name = "EPSILON-API-GATEWAY";
 const log = require('./api/v1/common/logs/logs.v1.services')
 global.globalLogger = log;
 
+const port = process.env.PORT || 3003;
+
+
+app.use('*', log.logRequest);
 /**
  * 
  */
+
+ app.use(cookieParser());
+
+ app.use(bodyParser.json({
+     limit: '5mb'
+ }));
+ 
+ app.use(bodyParser.urlencoded({
+     limit: '5mb',
+     extended: false
+ }));
 
 app.listen(port, () => console.log(`Server is listening on port ${port}!`))
 
