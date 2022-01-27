d286bd33e6882efa3e5360ca0366922291a69f96
ckenguva <ckenguva@iu.edu>
2022-01-26 23:21:38 -0500

    Added api to create PLOT

--- python_microservice/plotgraph/urls.py
+++ python_microservice/plotgraph/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('statuscheck/', views.statusCheck.as_view())
+    path('statuscheck/', views.statusCheck.as_view()),
+    #path('fetchplot2/', views.fetchPlot2.as_view()),
+    path('fetchplot/', views.fetchPlot.as_view())
 ]
