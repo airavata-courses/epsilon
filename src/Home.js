d9cb563255def438040e2e9de3cbfd89d6caeffa
ckenguva <ckenguva@iu.edu>
2022-02-05 21:29:50 -0500

    Copied from project1-chan

---
+++ src/Home.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { Typography, Container, AppBar, CssBaseline, Toolbar } from '@material-ui/core';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import WbSunnyTwoToneIcon from '@mui/icons-material/WbSunnyTwoTone';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { GoogleLogin } from "react-google-login";
+
+
+const theme = createTheme();
+
+const axiosApiCall = (url, method, body = {}) =>
+    axios({
+        method,
+        url: `${url}`,
+        data: body,
+        withCredentials: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+        },
+    });
+
+const Home = () => {
+    const navigate = useNavigate();
+
+    const ResponseGoogle = (response) => {
+        axiosApiCall("http://localhost:19030/auth/ms1/google/", "POST", response)
+            .then((res) => {
+                console.log(res);
+                makeNav();
+            })
+            .catch((err) => {
+                throw new Error(err);
+            });
+
+    };
+
+    function makeNav() {
+        navigate("../maphome", { replace: true });
+    }
+
+    return (
+        <>
+            <main>
+                <div>
+                    <ThemeProvider theme={theme}>
+                        <AppBar position="relative" style={{ background: '#990000' }}>
+                            <Toolbar>
+                                <WbSunnyTwoToneIcon style={{ marginRight: '20px' }} />
+                                <Typography variant="h5">Welcome to Epsilon</Typography>
+                            </Toolbar>
+                        </AppBar>
+
+                        <Container component="main" maxWidth="md" sx={{ marginTop: 8 }} >
+                            <CssBaseline />
+                            <Typography variant="h5" align="center" color="textPrimary" gutterBottom sx={{ fontStyle: 'italic' }}>
+                                "Reflectivity" is the amount of transmitted power returned to the radar receiver
+                                after hitting precipitation, compared to a reference power density at a distance
+                                of 1 meter from the radar antenna.
+                            </Typography>
+                        </Container>
+
+                        <Container component="main" maxWidth="xs" >
+                            <CssBaseline />
+                            <Box
+                                sx={{
+                                    marginTop: 8,
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    boxShadow: 4
+                                }}
+                            >
+                                <Avatar sx={{ m: 3 }} style={{ background: '#0066CC' }}>
+                                    <LockOutlinedIcon />
+                                </Avatar>
+                                <Typography variant="h4" align="center" color="textPrimary" gutterBottom>
+                                    Sign In
+                                </Typography>
+
+                                <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                                    Please sign in with your Google Account
+                                </Typography>
+                                <Box component="form" noValidate sx={{ mt: 1, mb: 3, boxShadow: 4 }}>
+                                    <GoogleLogin
+                                        clientId="636817888058-df41du2pgci4432ipd7b7afea6plq846.apps.googleusercontent.com"
+                                        buttonText="Login with Google"
+                                        onSuccess={ResponseGoogle}
+                                        cookiePolicy="single_host_origin"
+                                    />
+                                </Box>
+                            </Box>
+                        </Container>
+                    </ThemeProvider>
+                </div>
+            </main>
+        </>
+    )
+};
+
+export default Home;
---
+++ src/PlotResults.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import Header from "./Header";
+import ReactDOM from 'react-dom'
+import { Typography } from '@material-ui/core';
+
+const axiosApiCall = (url, method, body = {}) =>
+    axios({
+        method,
+        url: `${url}`,
+        data: body,
+        withCredentials: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json"
+        },
+    });
+
+const PlotResults = () => {
+    const FetchPlot = async () => {
+        const location = useLocation();
+        const requestBody = location.state.state || location.state
+        let res = await axiosApiCall("http://localhost:19030/api/gtw/v1/ms2/getNewImage", "POST", requestBody);
+        ReactDOM.render(<img src={`data:image/png;base64,${res.data}`} alt="Plot Results" />, document.getElementById("test"));
+    };
+    FetchPlot();
+    return (
+        <main>
+            <Header />
+            <Typography variant="h5">Please wait until the plot is generated......</Typography>
+            <br></br>
+            <div id="test" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}></div>
+
+        </main>
+    );
+}
+
+export default PlotResults;
