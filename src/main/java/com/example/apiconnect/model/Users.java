c83ee364a8150aa35daeb0b11293f9826c76255d
Pramod <pnbhakta@iu.edu>
2022-02-07 18:03:15 -0500

    java removed comments and added docker file

--- src/main/java/com/example/apiconnect/model/Users.java
+++ src/main/java/com/example/apiconnect/model/Users.java
@@ -1,38 +1,18 @@
 package com.example.apiconnect.model;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.mongodb.client.MongoCollection;
 import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 @Document(collection= "users")
 public class Users {
 
-    //Parameterized Constructor
-  /*  public Users(String id, String name, int salary)
-    {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-    }*/
-
-    //toString Method
-   /* public String toString()
-    {
-        return id+" "+name+" "+salary;
-    }*/
+   
     @Id
     @JsonIgnoreProperties
     private String id;
@@ -77,41 +57,5 @@ public class Users {
                 ", id='" + id + '\'' +
                 '}';
     }
-//    private String name;
-//    //Set
-//    public void setName(String name){
-//        this.name = name;
-//    }
-//    //Get
-//    public String getName(){
-//        return name;
-//    }
-//
-//    int salary;
-//    // Get
-//    public int getSalary()
-//    {
-//        return salary;
-//    }
-//    // Set
-//    public void setSalary(int salary)
-//    {
-//        this.salary = salary;
-//    }
 
-
-    // Extra attributes which are not in my model
-/*    private Map<String, Object> extra;
-
-    // Set
-    @JsonAnySetter
-    public void setExtra(final String key, final Object value){
-        this.extra.put(key, value);
-    }
-
-    // Get
-    @JsonAnyGetter
-    public Map<String, Object> getExtra() {
-        return Collections.unmodifiableMap(this.extra);
-    }*/
 }
--- src/main/java/com/example/apiconnect/repository/WeatherRepository.java
+++ src/main/java/com/example/apiconnect/repository/WeatherRepository.java
@@ -2,10 +2,6 @@ package com.example.apiconnect.repository;
 
 import com.example.apiconnect.model.Users;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface WeatherRepository extends MongoRepository<Users,String>{
    // List<Users> findAllUsers();
