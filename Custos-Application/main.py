0d7f4cf1941f446369275cd75d38b14d05f96183
Pramod <pnbhakta@iu.edu>
2022-05-02 15:45:05 -0400

    custos code

--- Custos-Application/main.py
+++ Custos-Application/main.py
@@ -6,10 +6,13 @@ from starlette.requests import Request
 from starlette.responses import Response
 from functools import lru_cache
 import config
+import custos
 
 app = FastAPI()
 
 # ENVIRONMENT VARIABLES
+
+
 @lru_cache()
 def get_settings():
     return config.Settings()
@@ -18,11 +21,10 @@ def get_settings():
 #SESSION_SERV_URL= get_settings().SESSION_SERV_URL
 
 
-
 #origins = [GATEWAY_SERV_URL]
 middleware = [Middleware(
     CORSMiddleware,
-    #allow_origins=origins,
+    # allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"], )
@@ -43,16 +45,15 @@ app = FastAPI()
 app = FastAPI(middleware=middleware)
 
 
-
 @app.get("/")
 def hello():
     return 'Hello'
 
 
-
 def start():
     print('CUSTOS API SERVICE')
     uvicorn.run(app, host="127.0.0.1", port=8888)
+
 
 if __name__ == '__main__':
     start()
